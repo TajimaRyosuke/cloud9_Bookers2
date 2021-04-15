1cffe70243ea6c37de13ceab36c31877d3ede4f4
TajimaRyosuke <east.helman56@gmail.com>
2021-04-15 15:33:20 +0000

    [update]4-9

---
+++ app/controllers/books_controller.rb
@@ -0,0 +1,37 @@
+class BooksController < ApplicationController
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
+  end
+
+
+  def edit
+  end
+
+  def update
+  end
+
+  def index
+    @books = PostImage.all
+  end
+
+  def show
+    @book = Book.find(params[:id])
+  end
+
+  def destroy
+    @book = book.find(params[:id])
+    @book.destroy
+    redirect_to books_path
+  end
+
+  private
+  def book_params
+    params.require(:book).permit(:book_name, :opinion)
+end
