fa411f5ebd5491002ee805751e633f5d9c62f80b
TajimaRyosuke <east.helman56@gmail.com>
2021-04-17 10:54:43 +0000

    [update]4-9完成度50%

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -7,31 +7,37 @@ class BooksController < ApplicationController
     @book = Book.new(book_params)
     @book.user_id = current_user.id
     @book.save
-    redirect_to book_path
+    redirect_to book_path(@book)
   end
 
 
   def edit
+    @book = Book.find(params[:id])
   end
 
   def update
+    @book = Book.find(params[:id])
+    @book.update(book_params)
+    redirect_to book_path(@book)
   end
 
   def index
-    @books = PostImage.all
+    @books = Book.all
   end
 
   def show
     @book = Book.find(params[:id])
+    @user = @book.user
   end
 
   def destroy
-    @book = book.find(params[:id])
-    @book.destroy
+    book = Book.find(params[:id])
+    book.destroy
     redirect_to books_path
   end
 
   private
   def book_params
     params.require(:book).permit(:book_name, :opinion)
+  end
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,10 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @books = @user.book.page(params[:page]).reverse_order
+    @books = @user.books.page(params[:page]).reverse_order
+    @book = Book.new
   end
+
   def edit
     @user = User.find(params[:id])
   end
@@ -13,8 +15,26 @@ class UsersController < ApplicationController
     redirect_to user_path(@user.id)
   end
 
+
+  def new
+    @book = Book.new
+  end
+
+  def create
+    @book = Book.new(book_params)
+    @book.user_id = current_user.id
+    @book.save
+    redirect_to book_path
+  end
+
+
   private
   def user_params
-    params.require(:user).permit(:name, :profile_image)
+    params.require(:user).permit(:name, :profile_image, :introduction)
   end
+
+  def book_params
+    params.require(:book).permit(:book_name, :opinion)
+  end
+
 end
