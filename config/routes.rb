4c43fd543d020ba814e80cc2de14bf234b5739d7
TajimaRyosuke <east.helman56@gmail.com>
2021-05-02 07:51:11 +0000

    [update]コメント機能搭載！

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root :to => 'homes#top'
   get "/home/about" => "homes#about", as:"home_about"
-  resources :books
+  resources :books do
+    resources:book_comments, only: [:create, :destroy]
+  end
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
