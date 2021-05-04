dcafe192f66c2f1df94bba1e8ad7a5aa92cce4de
TajimaRyosuke <east.helman56@gmail.com>
2021-05-04 14:14:06 +0000

    [update]フォロー機能搭載

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,6 +8,25 @@ class User < ApplicationRecord
   has_many :book_comments, dependent: :destroy
   has_many :favorites, dependent: :destroy
 
+  has_many :follower, class_name: "Relationship", foreign_key: "follower_id", dependent: :destroy
+  has_many :followed, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+
+  has_many :following_user, through: :follower, source: :followed
+  has_many :follower_user, through: :followed, source: :follower
+
+  def follow(user_id)
+    follower.create(followed_id: user_id)
+  end
+  def unfollow(user_id)
+    follower.find_by(followed_id: user_id).destroy
+  end
+  def following?(user)
+    following_user.include?(user)
+  end
+
+  has_many :passive_relationships, class_name: "Relationship", foreign_key: "followed_id", dependent: :destroy
+  has_many :followers, through: :passive_relationships, source: :follower
+
   attachment :profile_image
 
   validates :name, presence: true, uniqueness:true, length:{minimum:2,maximum:20}
--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,9 @@ Rails.application.routes.draw do
     resources:book_comments, only: [:create, :destroy]
   end
   resources :users
+  post 'follow/:id' => 'relationships#follow', as: 'follow' # フォローする
+  post 'unfollow/:id' => 'relationships#unfollow', as: 'unfollow' # フォロー外す
+  get '/users/:id/following(.:format)' => 'users#following', as: 'following_user'
+  get '/users/:id/followers(.:format)' => 'users#followers', as: 'followers_user'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
